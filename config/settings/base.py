c2be6e6ef7effc67d5b61ee161c4893ac3450869
Rafal Szymanski <r.szymansky@gmail.com>
2018-04-07 14:24:59 +0200

    added mvpcheap-style.css

--- config/settings/base.py
+++ config/settings/base.py
@@ -58,7 +58,9 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+     #'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+
     'django.contrib.humanize', # Handy template tags
     'django.contrib.admin',
 ]
